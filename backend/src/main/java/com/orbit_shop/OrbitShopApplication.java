bc4523a92aec633645f264ec850db66f9c00ae6d
Tiago <tiago.lobo@discente.ufg.br>
2026-04-19 17:20:22 -0300

    feat: setup Spring boot package structure

---
+++ backend/src/main/java/com/orbit_shop/OrbitShopApplication.java
@@ -0,0 +1,13 @@
+package com.orbit_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitShopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/orbit_shop/OrbitShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbit_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
