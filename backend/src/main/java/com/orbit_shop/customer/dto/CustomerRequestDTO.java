dab6a63479d0a5db696eaf548833df5e60f9c1b0
vavito <joaovictor2126@gmail.com>
2026-05-02 17:42:08 -0300

    refactor: dependência não utilizada removida

--- backend/src/main/java/com/orbit_shop/customer/dto/CustomerRequestDTO.java
+++ backend/src/main/java/com/orbit_shop/customer/dto/CustomerRequestDTO.java
@@ -3,7 +3,6 @@ package com.orbit_shop.customer.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import org.aspectj.weaver.ast.Not;
 import org.hibernate.validator.constraints.br.CPF;
 
 public record CustomerRequestDTO(
