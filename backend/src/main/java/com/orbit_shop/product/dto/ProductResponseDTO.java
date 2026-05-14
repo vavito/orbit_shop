72989851559e4bb445916ea15e5686cff2195b76
vavito <joaovictor2126@gmail.com>
2026-05-14 14:47:17 -0300

    refactor: linhas aleatórias removidas

--- backend/src/main/java/com/orbit_shop/product/dto/ProductResponseDTO.java
+++ backend/src/main/java/com/orbit_shop/product/dto/ProductResponseDTO.java
@@ -3,15 +3,9 @@ package com.orbit_shop.product.dto;
 import java.math.BigDecimal;
 
 public record ProductResponseDTO(
-
-<<<<<<< HEAD
-        Integer id,
-=======
         Long id,
->>>>>>> 9692f112925f9012e909702f67b8ac965ad528ce
         String name,
         String description,
         BigDecimal price,
         int quantity
-
 ) {}
